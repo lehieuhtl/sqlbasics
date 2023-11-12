750b2c6bd0686025872a3719f1e91ab6060a3c52
Hieu Le <lehieu.htl@outlook.com>
2023-11-12 03:47:39 -0800

    HiddenWorlds

--- -- @block.sql
+++ -- @block.sql
@@ -27,3 +27,19 @@ ORDER BY ID ASC
 LIMIT 2;
 -- @block
 CREATE INDEX email_index ON Users(email);
+
+-- @block
+CREATE TABLE HiddenWorlds(
+    id INT AUTO_INCREMENT,
+    street VARCHAR(255),
+    owner_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES Users(id)
+);
+-- @block
+INSERT INTO HiddenWorlds (owner_id, street)
+VALUES 
+    (1, 'san diego sail'),
+    (1, 'gp smol home'),
+    (1, 'arlington mansion'),
+    (1, 'irving box');
