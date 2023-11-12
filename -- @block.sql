3fadc345953dd30a824d5c20816bd31fecc25340
Hieu Le <lehieu.htl@outlook.com>
2023-11-12 03:34:00 -0800

    Initial commit

---
+++ -- @block.sql
@@ -0,0 +1,27 @@
+-- @block
+CREATE TABLE Users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    bio TEXT,
+    country VARCHAR(2)
+);
+-- @block
+INSERT INTO Users (email, bio, country)
+VALUES
+('hello@world.com','i am so tired','US'),
+('hellos@world.com','i am so tired','US'),
+('tom@field.com','i am tom field','US'),
+('broom@stick.com','sweep sweep','TX')
+-- @block
+INSERT INTO Users (email, bio, country)
+VALUES 
+('froom@stick.com','sweeeep','TX')
+-- @block
+SELECT email, id FROM Users
+
+WHERE country = 'TX'
+AND email LIKE 'b%'
+OR email LIKE 'f%'
+
+ORDER BY ID ASC
+LIMIT 2;
